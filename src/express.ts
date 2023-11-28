dfb0bfe6e91198406937939f57c6db0bed7afd57
Tilen Komel <tilen.komel10@gmail.com>
2023-11-28 17:29:35 +0100

    Updated express and http

--- src/express.ts
+++ src/express.ts
@@ -1,16 +1,7 @@
 import Express from "express"
-import * as settings from "./settings.js"
 
 export const express = Express()
 express.use(Express.json())
 express.use(Express.urlencoded({ extended: true }))
-
-express.listen(settings.HTTP_PORT, () => {
-    console.log(`
-    .- - - - - - - - - - - - - - - - - - - -
-    |   Started the API http server on port ${settings.HTTP_PORT}
-    '- - - - - - - - - - - - - - - - - - - -
-    `)
-})
 
 export default express
