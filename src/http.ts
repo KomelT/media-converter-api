202ed12c26400fba29590eabba51aea51c8ea2f5
Tilen Komel <tilen.komel10@gmail.com>
2023-11-28 18:05:19 +0100

    Updated http.ts

--- src/http.ts
+++ src/http.ts
@@ -9,9 +9,9 @@ addExitHandler(() => http.close(), "graceful http server shutdown")
 http.on("request", express)
 
 http.listen(settings.HTTP_PORT, () => {
-    console.log(`
-    .- - - - - - - - - - - - - - - - - - - -
-    |   Started the API http server on port ${settings.HTTP_PORT}
-    '- - - - - - - - - - - - - - - - - - - -
+  console.log(`
+.- - - - - - - - - - - - - - - - - - - -
+|   Started the API http server on port ${settings.HTTP_PORT}
+'- - - - - - - - - - - - - - - - - - - -
     `)
 })
