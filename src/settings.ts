793c6f9247bf85a6808adf3926d8294eab470d1a
Tilen Komel <me@komelt.dev>
2023-12-05 19:27:42 +0100

    Updated auth

--- src/settings.ts
+++ src/settings.ts
@@ -2,24 +2,21 @@ import Yaml from "yaml"
 
 if (!process.env.AUTHORIZED_API_KEYS) throw new Error("AUTHORIZED_API_KEYS environment variable is required!")
 {
-    const parsed = Yaml.parse(process.env.AUTHORIZED_API_KEYS)
-    if (!(parsed instanceof Object)) throw new Error("AUTHORIZED_API_KEYS must be an array of items!")
-    for (const [key, item] of Object.entries(parsed) as [string, any]) {
-        if (!(item instanceof Object)) throw new Error(`AUTHORIZED_API_KEYS[${key}] must be an object!`)
-        if (typeof key != "string") throw new Error(`AUTHORIZED_API_KEYS[${key}] must be a string!`)
-        if (typeof item.name != "string") throw new Error(`AUTHORIZED_API_KEYS[${key}].name must be a string!`)
-        if (typeof item.secret != "string") throw new Error(`AUTHORIZED_API_KEYS[${key}].secret must be a string!`)
-        if (typeof item.description != "string") throw new Error(`AUTHORIZED_API_KEYS[${key}].description must be a string!`)
-        if (key.length < 20) throw new Error(`AUTHORIZED_API_KEYS[${key}] must be long at least 20 characters!`)
-        if (item.secret.length < 30) throw new Error(`AUTHORIZED_API_KEYS[${key}].secret must be long at least 30 characters!`)
-    }
+  const parsed = Yaml.parse(process.env.AUTHORIZED_API_KEYS)
+  if (!(parsed instanceof Object)) throw new Error("AUTHORIZED_API_KEYS must be an array of items!")
+  for (const [id, item] of Object.entries(parsed) as [string, any]) {
+    if (!(item instanceof Object)) throw new Error(`AUTHORIZED_API_KEYS[${id}] must be an object!`)
+    if (typeof id != "string") throw new Error(`AUTHORIZED_API_KEYS[${id}] must be a string!`)
+    if (typeof item.name != "string") throw new Error(`AUTHORIZED_API_KEYS[${id}].name must be a string!`)
+    if (typeof item.description != "string") throw new Error(`AUTHORIZED_API_KEYS[${id}].description must be a string!`)
+    if (id.length < 30) throw new Error(`AUTHORIZED_API_KEYS[${id}] must be long at least 30 characters!`)
+  }
 }
 export type AuthorizedApiKeysItem = {
-    name: string
-    secret: string
-    description: string
+  name: string
+  description: string
 }
-export const AUTHORIZED_API_KEYS: { [key: string]: AuthorizedApiKeysItem } = Yaml.parse(process.env.AUTHORIZED_API_KEYS)
+export const AUTHORIZED_API_KEYS: { [id: string]: AuthorizedApiKeysItem } = Yaml.parse(process.env.AUTHORIZED_API_KEYS)
 
 if (!process.env.PUBLIC_URL_ROOT) throw new Error("PUBLIC_URL_ROOT environment variable is required!")
 export const PUBLIC_URL_ROOT: string = process.env.PUBLIC_URL_ROOT
