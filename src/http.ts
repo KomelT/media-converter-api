9c6365ee0a60e0eb307840edee6404acd02bb10b
Tilen Komel <me@komelt.dev>
2023-12-11 18:50:49 +0100

    Updated logging

--- src/http.ts
+++ src/http.ts
@@ -10,8 +10,8 @@ http.on("request", express)
 
 http.listen(settings.HTTP_PORT, () => {
   console.log(`
-.- - - - - - - - - - - - - - - - - - - -
-|   Started the API http server on port ${settings.HTTP_PORT}
-'- - - - - - - - - - - - - - - - - - - -
+    .- - - - - - - - - - - - - - - - - - - -
+    |   Started the API http server on port ${settings.HTTP_PORT}
+    '- - - - - - - - - - - - - - - - - - - -
     `)
 })
--- src/sharp.ts
+++ src/sharp.ts
@@ -2,4 +2,11 @@
 
 import sharp from "sharp"
 
+console.log(`
+    .- - - - - - - - - - - - - - - - - - - -
+    |   Sharp uses ${sharp.concurrency(0)} threads
+    |   Liborc is ${sharp.simd() ? "" : "not "}enabled
+    '- - - - - - - - - - - - - - - - - - - -
+    `)
+
 export default sharp
