68c8daaabee3ec245eaf183ccaa8dd9727769199
Tilen Komel <me@komelt.dev>
2023-12-11 14:31:42 +0100

    Add incomming and outcomming logging

--- src/express.ts
+++ src/express.ts
@@ -1,5 +1,6 @@
 import Express, { Request, Response, NextFunction } from "express"
 import multer from "multer"
+import { logIncomingRequest, logOutcommingRequest } from "./middlewares/logRequest.js"
 
 declare global {
   namespace Express {
@@ -13,6 +14,8 @@ declare global {
 export const express = Express()
 express.use(Express.json())
 express.use(Express.urlencoded({ extended: true }))
+express.use(logIncomingRequest)
+express.use(logOutcommingRequest)
 
 export default express
 export { Request, Response, NextFunction }
